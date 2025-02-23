2e3b103cd5fdb38daf23aa573934e7338f00ae67
Anes Hrvačić <anes.hrvacic@edu.fit.ba>
2025-02-23 18:23:46 +0100

    Removed UI, starting from scratch

--- Melo/Melo.UI/melo_mobile/lib/core/constants/api_constants.dart
+++
@@ -1,5 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = "http://10.0.2.2:7286";
-  static const String register = "$baseUrl/api/auth/register";
-  static const String login = "$baseUrl/api/auth/login";
-}
